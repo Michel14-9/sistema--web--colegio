2c07dbe2c7e85cb3b946aae2f97ab071358e83ca
francis <francisvelarde559@gmail.com>
2026-05-09 17:11:14 -0500

    revision de EstudianteController

--- src/main/java/com/universidad/sistema_academico/controller/EstudianteController.java
+++ src/main/java/com/universidad/sistema_academico/controller/EstudianteController.java
@@ -24,8 +24,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
 /**
  * Controlador unificado para Estudiantes.
  * Maneja tanto vistas MVC como API REST.
